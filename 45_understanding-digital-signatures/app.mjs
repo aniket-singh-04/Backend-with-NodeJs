79b380cbb3d3b30c9112dd69c0c544bcd57efff2
Aniket Singh <anonymousboterror0101@gmail.com>
2026-01-07 14:48:40 +0530

    normal hash vs signature

--- 45_understanding-digital-signatures/app.mjs
+++ 45_understanding-digital-signatures/app.mjs
@@ -2,6 +2,12 @@ import crypto from "node:crypto";
 import { createWriteStream } from "node:fs";
 import { readFile } from "node:fs/promises";
 
+// time consume create hash for all and then match for every time if check if we need 
+const filecontents = await readFile("path")
+const hashh = crypto.createHash("sha256").update(filecontents).digest("hex")
+
+
+// New
 const fileContent = await readFile("loan-agreement.md");
 const mySecretKey = "my-super-secret-key";
 
