da65c36fc530c36fa0330597d05f58fbba2aed8a
Aniket Singh <149407534+aniket-singh-04@users.noreply.github.com>
2026-01-17 12:14:12 +0530

    using auth library generate the auth url

---
+++ 72_using-google-auth-library-to-generate-authUrl/services/googleAuthService.js
@@ -0,0 +1,33 @@
+import { OAuth2Client } from "google-auth-library";
+
+const clientId =
+  "594555697613-18p3s2o6hl7mvc3gj0o2a2bg7b27tj9m.apps.googleusercontent.com";
+const clientSecret = "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";
+const redirectUrl = "http://localhost:4000/auth/google/callback";
+
+const client = new OAuth2Client({
+  clientId,
+  clientSecret,
+  redirectUri: redirectUrl,
+});
+
+export function generateGoogleAuthUrl() {
+  return client.generateAuthUrl({
+    scope: ["email", "profile", "openid"],
+  });
+}
+
+export async function fetchUserFromGoogle(code) {
+  console.log("Running fetchIdToken function...");
+
+  const { tokens } = await client.getToken(code);
+
+  const loginTicket = await client.verifyIdToken({
+    idToken: tokens.id_token,
+    audience: clientId,
+  });
+
+  const userData = loginTicket.getPayload();
+  console.log(userData);
+  return userData;
+}
