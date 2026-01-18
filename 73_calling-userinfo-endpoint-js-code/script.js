af0f1da5ec9e21890138887c0383b76c2f42251c
Aniket Singh <149407534+aniket-singh-04@users.noreply.github.com>
2026-01-18 10:54:41 +0530

    calling user info endpoint

---
+++ 73_calling-userinfo-endpoint-js-code/script.js
@@ -0,0 +1,13 @@
+const accessToken ="your access token";
+const response = await fetch(
+  `https://openidconnect.googleapis.com/v1/userinfo?access_token=${accessToken}`
+  // {
+  //   headers: {
+  //     Authorization: `Bearer ${accessToken}`,
+  //   },
+  // }
+);
+
+const userData = await response.json();
+
+console.log(userData);
